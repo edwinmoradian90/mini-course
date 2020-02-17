88624c933a64ad830ab31554ba031f0b0604a207
Edwin Moradian <edwin.moradian90@gmail.com>
2020-02-16 22:26:09 -0800

    added rails with axios for data to frontend

--- app/javascript/components/Home/Home.js
+++ app/javascript/components/Home/Home.js
@@ -1,19 +1,35 @@
 import React, { Component } from 'react'
 import Jumbotron from './Jumbotron'
 import Table from './Table/Table'
+import axios from 'axios'
 
 class Home extends Component {
   constructor() {
     super()
 
     this.state = {
-      course_modules: [
-        {id:1, title: '1. Setting up a new Ruby on Rails App with React.', description: 'lorem', active: false},
-        {id:2, title: '2. Adding React to an Existing Rails App.', description: 'lorem', active: false},
-        {id:3, title: '3. Building a Hello World App.', description: 'lorem', active: false},
-        {id:4, title: '4. Adding React Router Dom to your App.', description: 'lorem', active: false},
-      ]
+      course_modules: []
     }
+  }
+
+  componentDidMount = () => {
+     axios.get('/episodes.json')
+      .then(episode => {
+        debugger
+        let res = []
+        episode.data.data.map(episode => {
+          res.push({
+            id: episode.id, 
+            title: episode.title,
+            description: episode.description,
+            active: false
+          })
+          this.setState({ course_modules: res })
+        })
+      })
+      .catch(episode => {
+        debugger
+      })
   }
 
   handleVideoChange = (item, event) => {
